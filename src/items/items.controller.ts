b4469c54c38551145d6987b918cfb9c5fa1527cc
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2023-03-18 00:10:59 +0900

    商品新規作成機能の実装

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { ItemsService } from './items.service';
+import { Item } from './item.model';
+import { ItemStatus } from './item-status.enum';
 
 @Controller('items')
 export class ItemsController {
@@ -8,5 +10,22 @@ export class ItemsController {
   @Get()
   findAll() {
     return this.itemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('description') description: string,
+    @Body('price') price: number,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      description,
+      price,
+      status: ItemStatus.ON_SALE,
+    };
+    return this.itemsService.create(item);
   }
 }
