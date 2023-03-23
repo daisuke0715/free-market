e7e9d0d5c2bdb5cc526bbc68e958d6a45d86cda9
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2023-03-23 22:22:44 +0900

    Authモジュールを作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './features/items/items.module';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './features/auth/auth.module';
 
 @Module({
-  imports: [ItemsModule, DatabaseModule],
+  imports: [ItemsModule, DatabaseModule, AuthModule],
   controllers: [],
   providers: [],
 })
