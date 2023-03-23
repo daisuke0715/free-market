d067ae5138533f84a0104abb7e2c1f8a0c198a7a
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2023-03-23 23:23:35 +0900

    UserServiceのInterfaceを実装

--- src/features/auth/inteface/user-service.ts
+++ src/features/auth/inteface/user-service.ts
@@ -1,11 +1,13 @@
 import { User } from 'src/entities/user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
+import { CredentialsDto } from '../dto/credentials.dto';
 
 export interface IUserService {
+  signup(param: CreateUserDto): Promise<User>;
+  signin(param: CredentialsDto): Promise<{ accessToken: string }>;
   getUsers(): Promise<User[]>;
   getUser(id: string): Promise<User>;
-  create(param: CreateUserDto): Promise<User>;
   update(id: string, param: UpdateUserDto): Promise<User>;
   delete(id: string): Promise<void>;
 }
